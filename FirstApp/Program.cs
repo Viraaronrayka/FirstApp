e0f35f6799180d995a0c5304c7e3fdcbb60438dd
stockkaran <stockkaran@DESKTOP-04574S3>
2024-09-25 19:49:41 +0330

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+File.WriteAllText("D:\\other\\myFile.txt", "Hello");
+
+List<string> sentences = new List<string>();
+sentences.Add("Hello");
+sentences.Add("this is second line");
+sentences.Remove("Hello");
+File.AppendAllLines("D:\\other\\myFile.txt", sentences);
+
+string fileContents = File.ReadAllText("D:\\other\\myFile.txt", Encoding.UTF8);
+Console.WriteLine(fileContents);
